b00d0fe03cd99db5ff23acc2bb63b5d42feb997a
Kenji Suzuki <41730006+suzulabo@users.noreply.github.com>
2025-06-09 16:40:32 +0900

    Add pages page

---
+++ packages/web-notification/src/routes/pages/[slug]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { PageProps } from './$types';
+
+  let { data }: PageProps = $props();
+</script>
+
+<div class="container">
+  {data.slug}
+</div>
+
+<style lang="scss">
+  .container {
+    margin: 20svh auto;
+    width: min-content;
+    font-size: 32px;
+  }
+</style>
